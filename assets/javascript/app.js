24abbe0a83c269f2fe47d461600eed3382a67a24
Gage D'Orlando <gagedorlando@gmail.com>
2019-09-26 18:34:41 -0400

    added a bunch of stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,13 @@
 var totalExpenses=0;
-var expenses=[];
-var monthlyIncome;
+var expenses=JSON.parse(localStorage.getItem('expenses'));
+console.log(expenses)
+var monthlyIncome=localStorage.getItem('income');;
 var moneyLeft;
+var savePercent = localStorage.getItem('savePercent');
+$('#savingSlider').val(savePercent)
+$('#savingInput').val(savePercent)
+$('#income').val((monthlyIncome/parseFloat($('#frequency').val())))
+
 function otherExpense(){
     if($('#expenses').val()=='Other'){
         $('#other').css('display','inline')
@@ -13,11 +19,22 @@ function otherExpense(){
 function setIncome(){
     if($('#income').val()!=''){
         monthlyIncome = $('#income').val()*$('#frequency').val();
-        moneyLeft = (monthlyIncome-totalExpenses);
-        $('#moneyLeft').text('You have $'+moneyLeft.toFixed(2)+' left this month');
+        
+        printIncome();
+        localStorage.setItem('income',monthlyIncome);
     }
     else{
         $('#moneyLeft').text('');
+       
+    }
+}
+function printIncome(){
+    if(!isNaN(parseFloat(monthlyIncome))){
+    moneyLeft = (monthlyIncome-totalExpenses);
+    $('#moneyLeft').html('Your monthly income is $'+parseFloat(monthlyIncome).toFixed(2)+ '<br>You have $'+moneyLeft.toFixed(2)+' left this month');
+    }
+    else{
+        $('#moneyLeft').html('Please set an income');
     }
 }
 function addExpense(){
@@ -35,22 +52,28 @@ function addExpense(){
             console.log(expenses[i])
         }
     }
+    localStorage.setItem('expenses', JSON.stringify(expenses));
     logExpenses();
     setIncome();
+    calcSavings();
 }
 function logExpenses(){
     totalExpenses=0;
     $('#expenseList').html('')
     for(i in expenses){
         var amount =  expenses[i].amount;
-        totalExpenses+=parseInt(amount);
-        $('#expenseList').append($('<p>').html('<button id='+i+' class=\'remThis btn-outline-danger btn btn-sm\'>X</button> '+expenses[i].name+': $'+parseInt(amount).toFixed(2)))
+        totalExpenses+=parseFloat(amount);
+        
+        $('#expenseList').append($('<p>').html('<button id='+i+' class=\'remThis btn-outline-danger btn btn-sm\'>X</button> '+expenses[i].name+': $'+parseFloat(amount).toFixed(2)))
     }
 }
 $(document).on('click','.remThis',function(){
     expenses.splice($(this).attr('id'),1);
     logExpenses();
     setIncome();
+    calcSavings();
+    localStorage.setItem('expenses', JSON.stringify(expenses));
+    console.log(localStorage.getItem('expenses'))
 })
 function setSaveInput(){
     $('#savingInput').val($('#savingSlider').val())
@@ -67,7 +90,19 @@ function setSaveSlider(){
     calcSavings()
 }
 function calcSavings(){
-   var saveThis= (moneyLeft*($('#savingSlider').val()/100)).toFixed(2);
-    $('#theSavings').text('Put $'+saveThis+' into savings')
+    if(!isNaN(parseFloat(monthlyIncome))){
+    localStorage.setItem('savePercent', $('#savingSlider').val());
+    savePercent=localStorage.getItem('savePercent')
+    console.log(savePercent)
+   var saveThis= (moneyLeft*(savePercent/100)).toFixed(2);
+    $('#theSavings').text('Put $'+saveThis+' into savings this month')
     console.log(moneyLeft)
+    }
+    else{
+        $('#theSavings').text('Make sure you have filled in an income and percent')
+    }
 }
+
+logExpenses();
+printIncome();
+calcSavings();
